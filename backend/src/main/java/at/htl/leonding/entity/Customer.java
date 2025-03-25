88191adf150bc74656a62c9d701de0968afe4723
ebrandstaetter <4.brandst.e@gmail.com>
2025-03-25 10:35:29 +0100

    fixed customer, extended import.sql

--- backend/src/main/java/at/htl/leonding/entity/Customer.java
+++ backend/src/main/java/at/htl/leonding/entity/Customer.java
@@ -3,12 +3,15 @@ package at.htl.leonding.entity;
 import at.htl.leonding.entity.enumerations.Genre;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 
 import java.time.LocalDate;
 
 @Entity
 public class Customer extends Person {
 
+    @Enumerated(EnumType.STRING)
     @Column
     private Genre favoriteGenre;
 
