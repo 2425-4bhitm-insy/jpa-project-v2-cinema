a676207ec139ec77c2bff64c9e7e1807335e5c54
Jakob564 <j.klaffenboeck@students.htl-leonding.ac.at>
2025-03-25 09:23:19 +0100

    change title

--- backend/src/main/java/at/htl/leonding/entity/ScreeningRoom.java
+++ backend/src/main/java/at/htl/leonding/entity/ScreeningRoom.java
@@ -1,4 +1,25 @@
 package at.htl.leonding.entity;
 
+import jakarta.persistence.*;
+
+@Entity
 public class ScreeningRoom {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private int roomNumber;
+
+    @Column
+    private int seats;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
