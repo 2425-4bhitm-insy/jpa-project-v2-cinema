cdb795defe37e51ee94ccdf5ee1e00013a039aad
ebrandstaetter <4.brandst.e@gmail.com>
2025-03-25 09:28:15 +0100

    fixed import.sql

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,3 +1,3 @@
-insert into movie1 (title, gerne, duration, ageRating, description)
-    values("Impetigore", "HORROR", 106, 18,
-           "Horror Movie about an abandoned house in an indonesian village");
+insert into movie1 (title, genre, duration, ageRating, description)
+    values('Impetigore', 'HORROR', 106, 18,
+           'Horror Movie about an abandoned house in an indonesian village');
