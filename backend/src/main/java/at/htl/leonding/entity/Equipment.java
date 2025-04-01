3ac9aba04f84f7c24281eeb7db3ee97001d57240
Atilla Simsek <a.simsek@students.htl-leonding.ac.at>
2025-04-01 09:44:35 +0200

    created the Equipment entity

--- backend/src/main/java/at/htl/leonding/entity/Equipment.java
+++ backend/src/main/java/at/htl/leonding/entity/Equipment.java
@@ -1,9 +1,52 @@
 package at.htl.leonding.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 
 @Entity
 public class Equipment {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Column
+    private String name;
+    @Column
+    private String description;
+    @Column
+    private double price;
+
+    @ManyToOne
+    @JoinColumn(name = "screening_room_id")
+    private ScreeningRoom screeningRoom;
+
+
+    public Equipment() {
+    }
+
+    public Equipment(String name, String description, double price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
